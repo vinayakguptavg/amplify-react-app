4c3c6e19018730325e37af10d373355b0b9f7399
vinayakguptavg <134046072+vinayakguptavg@users.noreply.github.com>
2023-05-20 11:06:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://forescribe.ai"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Good Stuff
+          Quick Test
         </a>
       </header>
     </div>
